437c03322f915491a3ba3aea9f81dbd7ff836977
Wojciech Stylok <wojciech@stylok.pl>
2019-07-04 04:36:26 +0200

    initial commit

---
+++ WorkingWithVisualStudio/Models/SimpleRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkingWithVisualStudio.Models
+{
+    public class SimpleRepository
+    {
+        private static SimpleRepository sharedRepository = new SimpleRepository();
+        private Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+        public static SimpleRepository SharedRepository => sharedRepository;
+
+        public SimpleRepository()
+        {
+            var initialItems = new[]
+            {
+                new Product{ Name = "Kajak", Price = 275M },
+                new Product{ Name = "Kamizelka ratunkowa", Price = 40.10M },
+                new Product{ Name = "Wiosło", Price = 98M },
+                new Product{ Name = "Kask", Price = 20M }
+            };
+            foreach(var p in initialItems)
+            {
+                AddProduct(p);
+            }
+        }
+
+        public IEnumerable<Product> Products => products.Values;
+
+        public void AddProduct(Product p) => products.Add(p.Name, p);
+    }
+}
