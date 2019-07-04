e7748d6b47a940e17118a35fe3416707e136a4c2
Wojciech Stylok <wojciech@stylok.pl>
2019-07-04 14:28:37 +0200

    static .css and .js files added

---
+++ WorkingWithVisualStudio/wwwroot/js/third.js
@@ -0,0 +1,5 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var element = document.createElement("p");
+    element.textContent = "To jest akapit z pliku third.js";
+    document.querySelector("body").appendChild(element);
+});
