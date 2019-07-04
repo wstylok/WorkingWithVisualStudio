5cb5fc7ce5f252a2f31c93f75a684f04fc96fb48
Wojciech Stylok <wojciech@stylok.pl>
2019-07-05 01:49:28 +0200

    .js and .css files bundled and minified

---
+++ WorkingWithVisualStudio/wwwroot/js/bundle.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var element = document.createElement("p");
+    element.textContent = "To jest akapit z pliku third.js";
+    document.querySelector("body").appendChild(element);
+});
+document.addEventListener("DOMContentLoaded", function () {
+    var element = document.createElement("p");
+    element.textContent = "To jest akapit z pliku fourth.js";
+    document.querySelector("body").appendChild(element);
+});
