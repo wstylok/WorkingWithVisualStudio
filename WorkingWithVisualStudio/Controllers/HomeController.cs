e6ee0b123a8ffec43e220693d0397b2f99d8ff45
Wojciech Stylok <wojciech@stylok.pl>
2019-07-05 03:32:39 +0200

    adding product is available, styled all with bootstrap

--- WorkingWithVisualStudio/Controllers/HomeController.cs
+++ WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -9,6 +9,18 @@ namespace WorkingWithVisualStudio.Controllers
 {
     public class HomeController : Controller
     {
+        SimpleRepository Repository = SimpleRepository.SharedRepository;
+
         public IActionResult Index() => View(SimpleRepository.SharedRepository.Products.Where(p => p?.Price < 50));
+
+        [HttpGet]
+        public IActionResult AddProduct() => View(new Product());
+
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            Repository.AddProduct(p);
+            return RedirectToAction("Index");
+        }
     }
 }
